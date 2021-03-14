ca9f4e3dfc7a1947a7e6bb1f4c974c545b09006c
Artem <artem-gavrilin-00@mail.ru>
2021-03-14 10:59:31 +0300

    Перемещен gitignore, добавлен класс Constants, загружен NATS

---
+++ Valuator/Constants.cs
@@ -0,0 +1,9 @@
+namespace Valuator
+{
+    public static class Constants
+    {
+        public static const textPrefix = "TEXT-";
+        public static const similarityPrefix = "SIMILARITY-";
+        public static const rankPrefix = "RANK-";
+    }
+}
