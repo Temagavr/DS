35d2d9d439f6c9e5db63442d189dbd4792cc0c22
Artem <artem-gavrilin-00@mail.ru>
2021-03-23 10:14:09 +0300

    Убраны запуски redis nginx и nats через script файл

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -1,7 +1,4 @@
-cd ../../nats-server-v2.1.9-windows-386
-start nats-server.exe
-
-cd ../DS/Common
+cd ../Common
 start dotnet build
 
 cd ../Valuator
@@ -11,9 +8,3 @@ start dotnet run --urls "http://localhost:5002"
 cd ../RankCalculator
 start dotnet run
 start dotnet run
-
-cd ../../../Redis-x64-3.2.100
-start redis-server.exe
-
-cd ../../DS/DS/nginx-1.19.7/nginx-1.19.7/
-start nginx.exe
