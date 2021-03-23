44f76d36441143e9499ee0e749ed579d2bee52d7
Artem <artem-gavrilin-00@mail.ru>
2021-03-23 13:33:21 +0300

    Убраны лишние остановки в скрипте stop.cmd. Изменен путь к nginx

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -8,3 +8,6 @@ start dotnet run --urls "http://localhost:5002"
 cd ../RankCalculator
 start dotnet run
 start dotnet run
+
+cd ../nginx
+start nginx.exe
--- scripts/stop.cmd
+++ scripts/stop.cmd
@@ -1,5 +1,3 @@
 taskkill /f /im Valuator.exe
 taskkill /f /im RankCalculator.exe
-taskkill /f /im redis-server.exe
 taskkill /f /im nginx.exe
-taskkill /f /im nats-server.exe
